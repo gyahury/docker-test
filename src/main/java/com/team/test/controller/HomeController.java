a47954117a9fe38b280d6a8f0ecc8b109dcbfeb9
gyahury <rudgns1243@naver.com>
2024-08-21 10:52:08 +0900

    test: 템플릿엔진 작동 확인

--- src/main/java/com/team/test/controller/HomeController.java
+++ src/main/java/com/team/test/controller/HomeController.java
@@ -1,13 +1,15 @@
 package com.team.test.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping({"","/"})
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("data","타임리프 작동 확인");
         return "home"; 
     }
 }
